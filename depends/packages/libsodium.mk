d7efc91377230f231b65844d004181a29281f5be
kevacoin <info@kevacoin.org>
2020-09-26 15:34:27 -0700

    Updated libsodium signature.

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -2,7 +2,7 @@ package=libsodium
 $(package)_version=1.0.18
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version)-stable.tar.gz
-$(package)_sha256_hash=2ef4304dd844e274d78dfcf0201e1ba1a312eaba135bdc8affc56a3d80b58593
+$(package)_sha256_hash=df10b463be4a0cde29772cbe4ae67b84abb17a91874e3c5f29b3a6b0798e5677
 
 define $(package)_set_vars
 $(package)_config_opts_mingw32+=CFLAGS="-Ofast -fomit-frame-pointer -march=pentium3 -mtune=westmere"
