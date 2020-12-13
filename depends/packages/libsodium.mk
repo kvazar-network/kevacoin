bd9bebe047bc7164c8b8bae0343d73cc26e52601
kevacoin <info@kevacoin.org>
2020-12-12 19:16:17 -0800

    Updated libsodium sha256 checksum.

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -2,7 +2,7 @@ package=libsodium
 $(package)_version=1.0.18
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version)-stable.tar.gz
-$(package)_sha256_hash=df10b463be4a0cde29772cbe4ae67b84abb17a91874e3c5f29b3a6b0798e5677
+$(package)_sha256_hash=7673be93a53f6dcfebc03122063ca65a26e2eea723788d1b826abe6d5fcf3837
 
 define $(package)_set_vars
 $(package)_config_opts_mingw32+=CFLAGS="-Ofast -fomit-frame-pointer -march=pentium3 -mtune=westmere"
