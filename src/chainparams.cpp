1b8a24ac88565fb3bf77c9918aad06eb885d7755
kvazar-network <kvazar-network@pool>
2024-04-27 19:25:25 +0300

    remove incorrect seed address

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -171,9 +171,6 @@ public:
         vSeeds.emplace_back("dnsseed.kevacoin.org");
         vSeeds.emplace_back("dnsseed.keva.one");
 
-        // Third party DNS
-        vSeeds.emplace_back("kex.keva.one"); // by https://bitcointalk.org/index.php?topic=5213046.msg63803088#msg63803088
-
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,45); // K
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,70); // V
