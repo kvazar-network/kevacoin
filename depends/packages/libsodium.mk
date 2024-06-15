8d7c6c0f2792241d3327627566b4f582f0d67caa
kvazar-network <kvazar-network@pool>
2024-06-15 16:06:41 +0300

    update libsodium-1.0.18-stable checksum (13-Sep-2023 14:13 version)

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -2,7 +2,7 @@ package=libsodium
 $(package)_version=1.0.18
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version)-stable.tar.gz
-$(package)_sha256_hash=9de063e03cf9316b5e0e8188bb4991027c88f82462146a5e49bb47f19d0cf6f7
+$(package)_sha256_hash=63944201537ad2fe0dd035729fba60c1e3bc357e8521468fa51ab0c4e928c9d8
 
 define $(package)_set_vars
 $(package)_config_opts_mingw32+=CFLAGS="-Ofast -fomit-frame-pointer -march=pentium3 -mtune=westmere"
