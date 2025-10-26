eaf55b55a14f9e5b1de0077cf86ddffec7d50ce9
kvazar-network <kvazar-network@project>
2025-10-26 18:28:40 +0200

    fix date format

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -618,7 +618,7 @@ QVariant TransactionTableModel::data(const QModelIndex &index, int role) const
             QDateTime date = QDateTime::fromTime_t(static_cast<uint>(rec->time));
             QString txLabel = walletModel->getAddressTableModel()->labelForAddress(QString::fromStdString(rec->address));
 
-            details.append(date.toString("M/d/yy HH:mm"));
+            details.append(date.toString("d/M/Y HH:mm"));
             details.append(" ");
             details.append(formatTxStatus(rec));
             details.append(". ");
